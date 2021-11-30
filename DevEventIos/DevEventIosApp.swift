cea417e63a90f8176f39b4073262a018bd6c0f6c
jisungbin <ji@sungb.in>
2021-11-30 23:39:10 +0900

    Initial Commit

---
+++ DevEventIos/DevEventIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevEventIosApp.swift
+//  DevEventIos
+//
+//  Created by Ji Sungbin on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct DevEventIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
